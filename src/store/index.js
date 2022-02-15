984a98c02e4ebb68b312a51af6d180f135a049f6
birnamwood <birnamwood19@gmail.com>
2022-02-15 18:30:42 +0900

    Storeのセクション作成用の処理を追加

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,28 @@
 import { createStore } from 'vuex'
 
-export default createStore({
+const savedSections = localStorage.getItem('board-sections')
+
+const store = createStore({
   state: {
+    sections: savedSections ? JSON.parse(savedSections): []
   },
   getters: {
   },
   mutations: {
+    ceateSection(state, payload) {
+      state.sections.push( {title: payload.title, tasks:[]})
+    }
   },
   actions: {
+    ceateSection(context, payload) {
+      context.commit('createSection', payload)
+    }
   },
   modules: {
   }
 })
+store.subscribe((mutation, state) => {
+  localStorage.setItem('board-sections', JSON.stringify(state.sections))
+})
+
+export default store
